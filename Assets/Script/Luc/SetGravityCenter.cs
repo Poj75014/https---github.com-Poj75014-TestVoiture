cc79ad8daad7df7a4f9d581d19998dce72ace286
Luc <lucasbarbe@hotmail.fr>
2017-11-21 04:37:42 +0100

    Update settings

---
+++ Assets/Script/Luc/SetGravityCenter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGravityCenter : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 m_gravityCenter;
+
+    private void FixedUpdate()//pas bien
+    {
+        GetComponent<Rigidbody>().centerOfMass = m_gravityCenter;
+    }
+}
---
+++ Assets/Script/Luc/Spoiler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spoiler : MonoBehaviour {
+
+    [SerializeField]
+    private AnimationCurve m_force;
+    [SerializeField]
+    private Rigidbody m_rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        m_rigidbody.AddForce(m_force.Evaluate(transform.InverseTransformVector(m_rigidbody.velocity).z * 3.6f) * new Vector3(0,1,0));
+
+    }
+}
