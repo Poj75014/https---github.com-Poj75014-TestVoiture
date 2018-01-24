186302480671ac200a597198eca58f29a6b89b1d
Gaoura <gaoura@outlook.com>
2018-01-24 13:38:38 +0100

    Scène et scripts de Dash Florian

---
+++ Assets/Script/Flo/CubeGround.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class CubeGround : MonoBehaviour, IGround
+{
+    //private float groundDistance; //old
+    private Bounds bounds;
+
+    public bool IsGrounded()
+    {
+        // return Physics.Raycast(this.transform.position, Vector3.down, this.groundDistance + 0.1f); //old
+        return Physics.CheckCapsule(this.bounds.center, new Vector3(this.bounds.center.x, this.bounds.min.y - 0.1f, this.bounds.center.z), 0.18f);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        //this.groundDistance = this.GetComponent<Collider>().bounds.extents.y; //old
+        this.bounds = this.GetComponent<Collider>().bounds;
+    }
+}
---
+++ Assets/Script/Flo/SideDash.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(IGround), typeof(Rigidbody))]
+public abstract class SideDash : MonoBehaviour
+{
+    // ATTRIBUTES
+    public float speed;
+    public float dashAngleInDegrees;
+
+    protected IGround groundProperties;
+    
+    protected enum Direction { left = -1, right = 1 };
+    
+    // METHODS
+    protected abstract void Jump(Direction direction);
+    
+    protected virtual void Initialization()
+    {
+        this.groundProperties = this.gameObject.GetComponent(typeof(IGround)) as IGround;
+    }
+
+    protected virtual void ValidateAttributes()
+    {
+        this.dashAngleInDegrees = Mathf.Clamp(this.dashAngleInDegrees, 0f, 90f);
+        this.speed = Mathf.Max(this.speed, 0f);
+    }
+}
